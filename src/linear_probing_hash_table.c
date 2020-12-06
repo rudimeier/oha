6ec6764650767268483236c5acf47743259f1151
Andy Guenther <Andy.Guenther@rohde-schwarz.com>
2020-12-06 10:45:42 +0100

    replaced xxHASH by MumurHash

--- src/linear_probing_hash_table.c
+++ src/linear_probing_hash_table.c
@@ -7,11 +7,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <xxhash.h>
-
 #include "utils.h"
 
-#define XXHASH_SEED 0xc800c831bc63dff8
+#define HASH_SEED 0xc800c831bc63dff8
 
 #ifdef OHA_FIX_KEY_SIZE_IN_BYTES
 #if OHA_FIX_KEY_SIZE_IN_BYTES == 0
@@ -69,6 +67,54 @@ struct oha_lpht {
     bool clear_mode_on;
 };
 
+static inline uint64_t MurmurHash64A(const void * key, int len, uint64_t seed)
+{
+    const uint64_t m = 0xc6a4a7935bd1e995LLU;
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t * data = (const uint64_t *)key;
+    const uint64_t * end = (len >> 3) + data;
+
+    while (data != end) {
+        uint64_t k = *data++;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+
+    const unsigned char * data2 = (const unsigned char *)data;
+
+    switch (len & 7) {
+        case 7:
+            h ^= (uint64_t)(data2[6]) << 48;
+        case 6:
+            h ^= (uint64_t)(data2[5]) << 40;
+        case 5:
+            h ^= (uint64_t)(data2[4]) << 32;
+        case 4:
+            h ^= (uint64_t)(data2[3]) << 24;
+        case 3:
+            h ^= (uint64_t)(data2[2]) << 16;
+        case 2:
+            h ^= (uint64_t)(data2[1]) << 8;
+        case 1:
+            h ^= (uint64_t)(data2[0]);
+            h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
 static inline void * get_value(struct key_bucket * bucket)
 {
 #ifdef OHA_WITH_KEY_FROM_VALUE_SUPPORT
@@ -88,9 +134,9 @@ static uint64_t hash_key(struct oha_lpht * table, const void * key)
 {
 #ifdef OHA_FIX_KEY_SIZE_IN_BYTES
     (void)table;
-    return XXH64(key, OHA_FIX_KEY_SIZE_IN_BYTES, XXHASH_SEED);
+    return MurmurHash64A(key, OHA_FIX_KEY_SIZE_IN_BYTES, HASH_SEED);
 #else
-    return XXH64(key, table->config.key_size, XXHASH_SEED);
+    return MurmurHash64A(key, table->config.key_size, HASH_SEED);
 #endif
 }
 
